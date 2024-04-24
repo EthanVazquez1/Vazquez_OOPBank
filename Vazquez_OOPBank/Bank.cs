dac01b098e614c93b45be0df54ea21a10b15d8a7
Ethan Vazquez <Ethan.Vazquez050@gmail.com>
2024-04-24 13:59:40 -0700

    Add project files.

---
+++ Vazquez_OOPBank/Bank.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vazquez_OOPBank
+{
+    internal class Bank
+    {
+        private int balance;
+       public Bank(int amount)
+        {
+            balance = amount;
+        }
+        public int GetBalance()
+        {
+            return balance;
+        }
+
+        public int add(int amount)
+        {
+            return balance += amount;
+        }
+        public int subtract(int amount)
+        { 
+            if (amount > 500)
+            {
+                amount = 500;
+            }
+            if (balance-amount <= 0)
+            {
+                amount = balance;
+            }
+            balance -= amount;
+            return balance;
+        }
+    }
+}
---
+++ Vazquez_OOPBank/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Vazquez_OOPBank
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int startingAmount = 1000;
+
+            Bank Twinstar = new Bank(startingAmount);
+            bool bankActive = true;
+            do
+            {
+                Console.WriteLine("Your balance is " + Twinstar.GetBalance().ToString("C") + ", what would you like to do?");
+                Console.WriteLine("Your options are: 1.Deposit  2.Withdrawl  3.Check balance  4.Quit");
+                string answer = Console.ReadLine();
+
+
+                if (answer == "1")
+                {
+                    Console.WriteLine("How much would you like to deposit?");
+                    string addinput = Console.ReadLine();
+                    int addamount = int.Parse(addinput);
+                    Twinstar.add(addamount);
+                }
+                else if (answer == "2")
+                {
+                    Console.WriteLine("How much would you like to withdrawl?");
+                    string subtractinput = Console.ReadLine();
+                    int subtractamount = int.Parse(subtractinput);
+                    Twinstar.subtract(subtractamount);
+                }
+                else if (answer == "3")
+                {
+                    Console.WriteLine("Your current balance is " + Twinstar.GetBalance());
+                }
+                else if (answer == "4")
+                {
+                    bankActive = false;
+                }
+            } while (bankActive);
+        }
+    }
+}
